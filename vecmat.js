c1b70b0c97671cf08f7af0e878f31551c701ae50
Carlos Ureña <curena@ugr.es>
2020-06-09 09:56:06 +0200

    added 'perspective' and 'frustum' matrices

--- vecmat.js
+++ vecmat.js
@@ -1,3 +1,16 @@
+
+// ------------------------------------------------------------------------------------------------
+
+
+function N2S( x )
+{
+    let str = x.toLocaleString( 'EN', {minimumFractionDigits: 3, maximumFractionDigits: 3})
+    if ( x >= 0.0 )
+        str = ' '+str
+    return str
+}
+// ------------------------------------------------------------------------------------------------
+
 class Vec3 extends Float32Array
 {
     constructor( obj )
@@ -23,6 +36,7 @@ class Vec3 extends Float32Array
     g() { return this[1] }
     b() { return this[2] }
 }
+
 // ------------------------------------------------------------------------------------------------
 
 class Mat4 extends Float32Array 
@@ -48,9 +62,11 @@ class Mat4 extends Float32Array
     toString()
     {
         let str = '\n'
+        const n = 3
+
         for( let i = 0 ; i<4 ; i++ )
         {   const b = i*4
-            str = str + `   | ${this[b+0]}, ${this[b+1]}, ${this[b+2]}, ${this[b+3]} |\n`
+            str = str + `   | ${N2S(this[b+0])}, ${N2S(this[b+1])}, ${N2S(this[b+2])}, ${N2S(this[b+3])} |\n`
         }    
         return str    
     }
@@ -126,6 +142,149 @@ function Mat4_UndProj2D( sx, sy )
     ])
     
 }
+
+/**
+ // ---------------------------------------------------------------------
+
+Matriz4f MAT_Frustum( const float l, const float r, const float b, const float t, const float n, const float f )
+{
+   const float eps = 1e-6 ;
+   assert( fabs(r-l) > eps && fabs(t-b) > eps  && fabs(n-f) > eps );
+
+   const float
+      irl = 1.0f/(r-l) ,
+      itb = 1.0f/(t-b) ,
+      inf = 1.0f/(n-f) ;
+   const float
+      a0 = 2.0f*n*irl ,
+      a2 = (r+l)*irl,
+      b1 = 2.0f*n*itb ,
+      b2 = (t+b)*itb ,
+      c2 = (n+f)*inf ,
+      c3 = 2.0f*f*n*inf ;
+
+   Matriz4f
+      res ;
+
+   res(0,0) = a0  ; res(0,1) = 0.0; res(0,2) = a2  ; res(0,3) = 0.0 ;
+   res(1,0) = 0.0 ; res(1,1) = b1 ; res(1,2) = b2  ; res(1,3) = 0.0 ;
+   res(2,0) = 0.0 ; res(2,1) = 0.0; res(2,2) = c2  ; res(2,3) = c3  ;
+   res(3,0) = 0.0 ; res(3,1) = 0.0; res(3,2) = -1.0; res(3,3) = 0.0 ;
+
+   return res ;
+}
+
+**/
+
+// ---------------------------------------------------------------------
+
+/**
+ * Returns the OpenGL frustum matrix (http://docs.gl/gl2/glFrustum)
+ * @param {number} l 
+ * @param {number} r 
+ * @param {number} b 
+ * @param {number} t 
+ * @param {number} n 
+ * @param {number} f 
+ * @returns {number} a Mat4 object with the entries corresponding to glFrustum call.
+ */
+function Mat4_Frustum( l, r, b, t, n, f )
+{
+    const eps = 1e-6 
+    Check( Math.abs(r-l) > eps && Math.abs(t-b) > eps  && Math.abs(n-f) > eps );
+
+    const 
+        irl = 1.0/(r-l) ,
+        itb = 1.0/(t-b) ,
+        inf = 1.0/(n-f) ,
+        a0  = 2.0*n*irl,  a2 = (r+l)*irl,
+        b1  = 2.0*n*itb,  b2 = (t+b)*itb ,
+        c2  = (n+f)*inf,  c3 = 2.0*f*n*inf 
+
+    return new Mat4
+    ([  a0,    0.0,    a2,    0.0,
+        0.0,   b1,     b2,    0.0,
+        0.0,   0.0,    c2,    c3,
+        0.0,   0.0,   -1.0,   0.0 
+    ])
+}
+
+/** 
+
+// ---------------------------------------------------------------------
+
+Matriz4f MAT_Ortografica( const float l, const float r, const float b, const float t, const float n, const float f )
+{
+   const float eps = 1e-6 ;
+   assert( fabs(r-l) > eps && fabs(t-b) > eps  && fabs(n-f) > eps );
+
+   const float
+      irl = 1.0f/(l-r) ,
+      itb = 1.0f/(b-t) ,
+      inf = 1.0f/(n-f) ;
+   const float
+      a0 = -2.0f*irl ,
+      a3 = (r+l)*irl,
+      b1 = -2.0f*itb ,
+      b3 = (t+b)*itb ,
+      c2 = 2.0f*inf ,
+      c3 = (f+n)*inf ;
+
+   Matriz4f
+      res ;
+
+   res(0,0) = a0  ; res(0,1) = 0.0 ; res(0,2) = 0.0 ; res(0,3) = a3  ;
+   res(1,0) = 0.0 ; res(1,1) = b1  ; res(1,2) = 0.0 ; res(1,3) = b3  ;
+   res(2,0) = 0.0 ; res(2,1) = 0.0 ; res(2,2) = c2  ; res(2,3) = c3  ;
+   res(3,0) = 0.0 ; res(3,1) = 0.0 ; res(3,2) = 0.0 ; res(3,3) = 1.0 ;
+
+   return res ;
+}
+
+// ---------------------------------------------------------------------
+
+Matriz4f MAT_Perspectiva( const float fovy_grad, const float raz_asp, const float n, const float f )
+{
+   const float eps = 1e-6 ;
+   assert( raz_asp > eps && fovy_grad > eps  && fabs(n-f) > eps );
+
+   const float
+      fovy_rad = fovy_grad*2.0f*M_PI/360.0f,
+      t = n*tan(0.5*fovy_rad),
+      r = t/raz_asp,
+      b = -t ,
+      l = -r ;
+
+   return MAT_Frustum( l,r,b,t,n,f );
+}
+ */
+
+// ------------------------------------------------------------------------------------------------
+
+/**
+ * Returns a frustum perspective matrix, by using an alternative parameter set
+ * (see: https://stackoverflow.com/questions/16571981/gluperspective-parameters-what-do-they-mean)
+ * 
+ * @param {number} fovy_deg -- vertical field of view angle (in degrees) (0..180)
+ * @param {number} asp_rat  -- viewport aspect ratio (Y/X)  (>0)
+ * @param {number} n        -- distance from focus to near clipping plane (usually >0) 
+ * @param {number} f        -- distance from focus to far clipping plane (>n)
+ */
+function Mat4_Perspective( fovy_deg, asp_rat, n, f )
+{
+   const eps = 1e-6 
+   Check( asp_rat > eps && fovy_deg > eps  && Math.abs(n-f) > eps, 'Mat4_Perspective: invalid arguments' )
+
+   const 
+      fovy_rad = fovy_deg*2.0*Math.PI/360.0,
+      t = n*Math.tan( 0.5*fovy_rad ),
+      r = t/asp_rat,
+      b = -t ,
+      l = -r 
+
+   return Mat4_Frustum( l, r, b, t, n, f )
+}
+
 // ------------------------------------------------------------------------------------------------
 
 function TestVec3()
@@ -182,6 +341,9 @@ function TestMat4()
 
     const m9 = m6.compose( m7 )
     Log(`${fname} compose II: m9 == ${m9}`)
+
+    const mpersp = Mat4_Perspective( 90, 1.2, 0.1, 5.0 )
+    Log(`${fname} perspective: mpersp == ${mpersp}`)
 
     Log(`${fname} ends`)
 

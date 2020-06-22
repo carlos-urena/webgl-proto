8df415956e30f9704f9018748e5e69d37fca4047
Carlos Ureña <curena@ugr.es>
2020-06-22 14:11:28 +0200

    working on general PLY parsing (I)

--- ply-parse.js
+++ ply-parse.js
@@ -1,3 +1,324 @@
+// Info about PLY format
+// http://paulbourke.net/dataformats/ply/
+
+/**
+ * Parses the PLY header, returns an object 
+ * @param {Array<string>} lines        --  
+ * @param {number}        end_line_num 
+ */
+function ParsePLYHeader( lines, end_line_num )
+{
+    let result = 
+        {   parse_ok          : false, 
+            parse_message     : 'no errors found so far', 
+            pase_message_line : '',
+            elements          : new Map()  // elements array, indexed by element names 
+        }
+
+     // seek for 'end_header' line
+     let ehl = 2    // end header line num
+     while( ehl < lines.length && (lines[ehl]).trim() != 'end_header'   )
+         ehl ++ 
+
+    /// Check basic header properties
+ 
+    if ( lines.length == ehl )
+    {   result.parse_message = "end of header not found"
+        return result 
+    }
+    if ( lines.length < 3 )
+    {   result.parse_message = "Invalid file, it has less than 3 lines"
+        return result
+    }
+    if ( lines[0] != 'ply' )
+    {   result.parse_message = "Invalid header: first line is not exactly 'ply'"
+        return result
+    }
+    if ( lines[1] != 'format ascii 1.0' )
+    {   result.parse_message = "Invalid header: second line is not exactly 'format ascii 1.0'"
+        return result
+    }
+
+    /// Process header lines, check it is syntactically valid
+
+    for( let l = 3 ; l < ehl ; l++ )
+    {
+        const
+            line     = lines[l].trim(), 
+            tokens   = line.split(' ')
+
+        result.parse_message_line = `line ${l}: "${line}"`
+
+        // allow empty lines
+        if ( tokens.length == 0 )
+            continue 
+        if ( tokens[0] == 'comment' )
+            continue
+
+        if ( tokens[0] == 'element') // element description line
+        {
+            if ( tokens.length != 3 )
+            {   result.parse_message = "line starting with 'element' has invalid format (not 3 words)"
+                return result
+            }
+            let element = 
+            {   name       : tokens[1], 
+                num_lines  : parseInt( tokens[2], 10 ), 
+                properties : new Map()  // element properties, indexed by properties' names
+            }
+            if ( element.num_lines === NaN || element.num_lines <= 0  )
+            {   result.parse_message = "line starting with 'element' has invalid number of items"
+                return result
+            }
+            if ( results.element.has( element.name ) )
+            {
+                result.parse_message = "duplicate element name in two lines"
+                return result
+            }
+            result.elements.set( element.name, element )   
+        }
+        else if ( tokens[0] == 'property'  ) // property description line
+        {
+            let property = null 
+
+            if ( result.elements.length == 0 )
+            {   result.parse_message = "there is a 'property' line before any 'element' line"
+                return result
+            }
+            if ( tokens[1] != 'list' )  // property (not list)
+            {
+                if ( tokens.length != 3 )
+                {   result.parse_message = "line starting with 'property' (not list) has invalid format (not 3 words)"
+                    return result
+                }
+                property = 
+                {   is_list    : false,
+                    name       : tokens[2],  
+                    value_type : tokens[1]
+                }
+            }
+            else  // property list
+            {
+                if ( tokens.length != 5 )
+                {   result.parse_message = "line starting with 'property list' has invalid format (not 5 words)"
+                    return result
+                }
+                property = 
+                {   is_list     : true,
+                    name        : tokens[4],  
+                    value_type  : tokens[3],
+                    length_type : tokens[2]
+                }
+            }
+            let properties = result.elements[ result.elements.length-1 ].properties
+
+            if ( properties.has( property.name ) )
+            {
+                result.parse_message = "duplicate property name in two lines of an element description in header"
+                return result
+            }
+            properties.set( property.name, property )
+        }
+        
+    }
+
+
+
+
+    result.parse_ok = true 
+}
+
+// -------------------------------------------------------------------------------------------------
+
+bool debug_ply_parse = true 
+
+/**
+ * Parses the lines of a PLY file, this version 
+ *   - reads the elements and elements properties
+ *   - expects at least 'vertex' elements with 'x','y','z' floating point properties
+ *   - expects 'face' element with a list of integer indexes, each 'face' must have exactly 3 vertexes
+ *   - is able to process texture coords, vertex colors, normals .....(this is WIP)
+ * 
+ * returns an object with 2 arrays: vertex cooordinates, faces (triangles), (optionally: texture coords, etc...)
+ * 
+ * @param {Array<string>} lines -- the lines of a ply file. 
+ * @returns {object} -- the object includes these properties
+ *                        - parse_ok {bool} -- true iif there where no errors
+ *                        - parse_message  {string} -- resulting message, if 'parse_ok' is 'fals', contains error description.
+ *                        - vertex_coords  {Float32Array} -- vertex coordinates  ( 3 entries per vertex)
+ *                        - triangles_data {Uint32Array}  --  triangle coordinates (3 entries per face)
+ *                        - vertex_colors  {Uint8Array}  
+ *                        - vertex_texcoo  {Float32Array} --  
+ */
+
+
+function ParsePLYLines( lines )
+{
+    const fname = 'ParsePLYLines():'
+    if ( debug_ply_parse ) 
+        Log(`${fname} begins.`)
+
+    let result = { parse_ok: false, parse_message: 'no errors found so far' }
+    
+    
+    
+
+    const header = ParsePLYHeader( lines, ehl )
+    if ( ! header.parse_ok )
+        return header
+    
+    
+    // get number of vertexes and number of triangles
+    let num_verts      = 0,  
+        num_tris       = 0,
+        num_verts_line = 0,
+        num_tris_line  = 0
+
+    
+
+    if ( num_tris_line < num_verts_line )
+    {
+        result.parse_message = "'element face' before 'element vertex' (must be the other way around)"
+        return result
+    }
+    if ( num_verts < 3 )
+    {
+        result.parse_message = `number of vertexes must be 3 at least (it is ${num_verts})`
+        return result
+    }
+    if ( lines.length < ehl + num_tris + num_verts )
+    {
+        result.parse_message = `the number of lines is smaller than the required for header + vertexes + faces`
+        return result
+    }
+
+    //// TODO: check properties are as expected .......
+    
+    // we assume the header is ok. lets load 
+
+    if ( debug_mesh ) 
+    {
+        Log(`${fname} num_verts == ${num_verts}`)
+        Log(`${fname} num_tris  == ${num_tris}`)
+    }
+    // const max_num_tris = -1 // 100000
+    // if (  max_num_tris > 0 && num_tris >  max_num_tris )  // truncate number of vertexes .....??
+    //     num_tris = max_num_tris
+
+    coords_data     = new Float32Array( 3*num_verts )
+    triangles_data  = new Uint32Array( 3*num_tris )
+    colors_data     = new Float32Array( 3*num_verts )
+    
+    // load vertexes coords and  vertex colors
+
+    let iv = 0  // vertex index
+
+    for( let l = ehl+1 ; l < ehl+1+num_verts ; l++ )
+    {
+        const tokens = lines[l].trim().split(' ')
+        if ( tokens.length != 7 )
+        {
+            result.parse_message =  `vertex at line ${l}: expected 7 numbers but found ${tokens.length}\n`
+            result.parse_message += `line ${l} is: '${lines[l]}'`
+            return result
+        }
+        const 
+            x = -parseFloat( tokens[0] ),
+            y = parseFloat( tokens[2] ),   //// flip Y <-> Z ???
+            z = parseFloat( tokens[1] ),    //// flip and negate ???
+            r = parseInt( tokens[3] ),
+            g = parseInt( tokens[4] ),
+            b = parseInt( tokens[5] ),
+            a = parseInt( tokens[6] )
+
+        const p = 3*iv 
+        
+        coords_data[p+0] = x 
+        coords_data[p+1] = y
+        coords_data[p+2] = z 
+
+        colors_data[p+0] = r/255.0
+        colors_data[p+1] = g/255.0
+        colors_data[p+2] = b/255.0
+
+        iv++ 
+    }
+
+    let it = 0  // triangle number
+
+    for( let l = ehl+1+num_verts ; l < ehl+1+num_verts+num_tris ; l++ )
+    {
+        const tokens = lines[l].trim().split(' ')
+        if ( tokens.length != 4 )
+        {
+            result.parse_message = `face at line ${l}: expected 4 numbers but found ${tokens.length}\n`
+            result.parse_message += `line ${l} is: '${lines[l]}'`
+            return result
+        }
+        const nv = parseInt( tokens[0] )
+        if ( nv != 3 )
+        {
+            result.parse_message = `face at line ${l}: expected a face with 3 vertexes but found ${nv}\n`
+            result.parse_message += `line ${l} is: '${lines[l]}'`
+            return result
+        }
+        const 
+            i0 = parseInt( tokens[1] ),
+            i1 = parseInt( tokens[2] ),
+            i2 = parseInt( tokens[3] )
+
+        if ( i0 < 0 || num_verts < i0  || 
+             i1 < 0 || num_verts < i1  || 
+             i2 < 0 || num_verts < i2  )
+        {
+            result.parse_message = `face at line ${l}: a vertex index is out of range (indexes are: ${i0}, ${i1}, ${i2}), num verts is ${num_verts}\n`
+            result.parse_message += `line ${l} is: '${lines[l]}'`
+            return result
+        }
+        const p = 3*it 
+
+        triangles_data[p+0] = i0 
+        triangles_data[p+1] = i1
+        triangles_data[p+2] = i2 
+        
+        it++ 
+    }
+
+    const normalize_coords = true
+    
+    if ( normalize_coords )
+    {
+        // normalize coordinates to -1 to +1 
+        let   bbox   = ComputeBBox( coords_data )
+        const maxdim = Math.max( bbox.xmax-bbox.xmin, bbox.ymax-bbox.ymin, bbox.zmax-bbox.zmin ),
+              center = [ 0.5*(bbox.xmax+bbox.xmin), 0.5*(bbox.ymax+bbox.ymin), 0.5*(bbox.zmax+bbox.zmin) ],
+              scale  = 2.0/maxdim
+
+        if ( debug_mesh )
+        {  
+            Log( `${fname}: maxdim == ${maxdim}` )
+            Log( `${fname}: center == ${center}` )
+        }
+        for( let iv = 0 ; iv < num_verts ; iv++ )
+        {
+            const p = 3*iv
+            for( let ic = 0 ; ic < 3 ; ic++ )
+                coords_data[p+ic] = scale*( coords_data[p+ic] - center[ic] )
+        }
+    }
+    // done
+    result.coords_data = coords_data
+    result.colors_data = colors_data
+    result.triangles_data = triangles_data
+    result.parse_ok   = true // done!
+
+    return result
+}
+
+
+
+
+
 
 // -------------------------------------------------------------------------------------------------
 
